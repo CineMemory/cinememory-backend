d656a70fa3beed185e1e67ff3f387b412ac16311
se0hyun <be8shyn@gmail.com>
2025-05-23 13:45:11 +0900

    Remove: movies 모델 중개 테이블 삭제

--- movies/models.py
+++ movies/models.py
@@ -13,11 +13,3 @@ class Director(models.Model):
 
 class Actor(models.Model):
     name = models.CharField(max_length=255)
-
-class MovieDirector(models.Model):  # 영화-감독 중개테이블
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
-
-class MovieActor(models.Model):  # 영화-배우 중개테이블
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)

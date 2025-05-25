eee26fce1045d240ffb4cfe7681015f6ae59ca91
se0hyun <be8shyn@gmail.com>
2025-05-25 15:05:04 +0900

    Feat: 게시글 조회 로직 구현

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    path('', views.post_list, name='PosstList'),
     path('post/', views.create_post, name='createPost'),
 ]
 

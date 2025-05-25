4de72b571c1ea91011c57c56741eff8d22a482bb
se0hyun <be8shyn@gmail.com>
2025-05-25 18:39:26 +0900

    Chore: movies/serializer 필요 클래스 생성

--- movies/serializer.py
+++ movies/serializer.py
@@ -1,2 +1,24 @@
 from rest_framework import serializers
 from .models import Movie, Actor, Director, Series, Genre
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'

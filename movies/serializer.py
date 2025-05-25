a9d47ea109b4ebfe2aa30db69bce24685a78dd34
se0hyun <be8shyn@gmail.com>
2025-05-25 19:39:12 +0900

    Feat: 영화 상세 조회 기능 구현

--- movies/serializer.py
+++ movies/serializer.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import Movie, Actor, Director, Series, Genre
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
-
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
@@ -13,12 +8,26 @@ class ActorSerializer(serializers.ModelSerializer):
 
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Director
         fields = '__all__'
 
 class SeriesSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Series
         fields = '__all__'
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True, read_only=True)
+    directors = DirectorSerializer(many=True, read_only=True)
+    series = SeriesSerializer(read_only=True)  # ForeignKey이므로 many=True 제거
+    genres = GenreSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
         fields = '__all__'
